5a2674e4ff5b9b79ab0ff1eecbecbf84284006c4
Rajat Sharma <lunasunkaiser@gmail.com>
2019-10-09 01:04:42 +0530

    Added pics

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { Text } from '@elementary/components';
+import { Text, Grid } from '@elementary/components';
 import { Motion, spring } from 'react-motion';
 import { useState } from 'react';
 
@@ -8,20 +8,72 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Loader from './components/Loader';
 import Body from './components/Body';
+import Hover from './components/Hover';
 
-import './style.css';
+import './style.scss';
 
 function getSweaterText(temp) {
   if (temp > 5 && temp <= 20) {
-    return 'Woolen & Body Warmers recommended, you need your Coat if you are heading out';
+    return {
+      title:
+        'Woolen & Body Warmers recommended, you need your Coat if you are heading out',
+    };
   }
   if (temp > 20 && temp <= 25) {
-    return 'Light woolens recommended, like Hoodie, and yeah you guessed it: a Sweater maybe';
+    return {
+      title: 'Light woolens recommended, like:',
+      styles: [
+        {
+          name: 'Cropped Sweaters',
+          image:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZRc8WimEhRpIuzDzxKE5CV0ucqjbg_uRE_r6-l54ije_0lsARhw',
+        },
+        {
+          name: 'Light Scarves',
+          image:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1QBqa9Y-qvED5CJjXXztexCMBifllBfh7S1DkM_0iAaBJHRRzeQ',
+        },
+        {
+          name: 'Suede Shoes',
+          image:
+            'https://images.asos-media.com/products/selected-homme-suede-shoes-in-tan/12951564-1-sand?$XXL$&wid=513&fit=constrain',
+        },
+        {
+          name: 'Hoodies',
+          image:
+            'https://lsco.scene7.com/is/image/lsco/levis/clothing/194910037-front-pdp-lse.jpg?$grid_desktop_full$',
+        },
+      ],
+    };
   }
   if (temp > 25) {
-    return 'Cottons recommended, like a T-Shirt would do... maybe';
+    return {
+      title: 'Cottons recommended, like:',
+      styles: [
+        {
+          name: 'T shirts',
+          image:
+            'https://s3.thcdn.com/productimg/300/300/11863818-1524626802125162.jpg',
+        },
+        {
+          name: 'Polo',
+          image:
+            'https://cdn.shopify.com/s/files/1/0014/6217/3742/products/113044-8800A.2.INTERNATIONAL_BLUE_2d9858b1-c307-4d67-8a7d-f60ae41cf5c4_2000x.jpg?v=1558619132',
+        },
+        {
+          name: 'Shorts',
+          image:
+            'https://i1.adis.ws/i/lucky/7M20810_340_1/Saturday-Stretch-10-Short-340?$productMainDesktop$',
+        },
+        {
+          name: 'Shirts',
+          image:
+            'https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/7189947/2018/8/30/b0a17130-00b2-47dd-9acf-75fcdf7333111535614137835-Bene-Kleed-Men-Off-White--Blue-Slim-Fit-Printed-Casual-Shirt-3181535614137565-1.jpg',
+        },
+      ],
+    };
   }
-  return 'Where the Fuck are you?';
+  return { title: 'Where the Fuck are you?' };
 }
 
 const swtext = [
@@ -40,6 +92,10 @@ function App() {
   if (!data || error) {
     return <Loader error={error} />;
   }
+
+  const collection = getSweaterText(
+    Number(data.current_observation.condition.temperature),
+  );
 
   return (
     <>
@@ -67,10 +123,26 @@ function App() {
             </Motion>
           </Text>
           <Text f="30px" pt="20px">
-            {getSweaterText(
-              Number(data.current_observation.condition.temperature),
-            )}
+            Its Fall time. An excuse for a delightful thrifting jaunt to collect
+            seasonal staples that you wear twice and that then take up half your
+            drawer space for the rest of the year.
           </Text>
+          <Text f="30px" pt="20px">
+            {collection.title}
+          </Text>
+          {collection.styles && (
+            <Grid
+              gridTemplateColumns={({ theme }) =>
+                theme.breakpoints(['1fr', '1fr 1fr'])
+              }
+              gridRowGap="30px"
+              mt="30px"
+            >
+              {collection.styles.map(style => (
+                <Hover text={style.name} image={style.image} />
+              ))}
+            </Grid>
+          )}
         </Fold>
         <Fold withHeader>
           <Text
---
+++ src/components/Hover.js
@@ -0,0 +1,25 @@
+import { Text, Image, Flex } from '@elementary/components';
+import { Tooltip } from 'react-tippy';
+
+export default ({ text, image }) => (
+  <Flex>
+    <Tooltip
+      html={
+        <Flex
+          style={{
+            background:
+              'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAOklEQVQoU43MSwoAMAgD0eT+h7ZYaOlHo7N+DNHL2HAGgBWcyGcKbqTghTL4oQiG6IUpOqFEC5bI4QD8PAoKd9j4XwAAAABJRU5ErkJggg==',
+          }}
+          direction="column"
+          p="30px"
+        >
+          <Image src={image} height="300px" width="200px" className="contain" />
+        </Flex>
+      }
+    >
+      <Text f="27px" className="hoverable">
+        {text}
+      </Text>
+    </Tooltip>
+  </Flex>
+);
