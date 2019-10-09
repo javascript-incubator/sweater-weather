8bfa99ce94c43d4f20029c5c5ef220c7d0a5d108
Rajat Sharma <lunasunkaiser@gmail.com>
2019-10-09 07:59:49 +0530

    Styled image frame

--- src/components/Hover.js
+++ src/components/Hover.js
@@ -1,20 +1,30 @@
-import { Text, Image, Flex } from '@elementary/components';
+import { Text, Image, Flex, Relative, Absolute } from '@elementary/components';
 import { Tooltip } from 'react-tippy';
 
 export default ({ text, image }) => (
   <Flex>
     <Tooltip
       html={
-        <Flex
-          style={{
-            background:
-              'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAOklEQVQoU43MSwoAMAgD0eT+h7ZYaOlHo7N+DNHL2HAGgBWcyGcKbqTghTL4oQiG6IUpOqFEC5bI4QD8PAoKd9j4XwAAAABJRU5ErkJggg==',
-          }}
-          direction="column"
-          p="30px"
-        >
-          <Image src={image} height="300px" width="200px" className="contain" />
-        </Flex>
+        <Relative>
+          <Flex
+            style={{
+              background:
+                'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAOklEQVQoU43MSwoAMAgD0eT+h7ZYaOlHo7N+DNHL2HAGgBWcyGcKbqTghTL4oQiG6IUpOqFEC5bI4QD8PAoKd9j4XwAAAABJRU5ErkJggg==',
+            }}
+            direction="column"
+            height="300px"
+            width="200px"
+          >
+            <Absolute zIndex={1} top="10px" left="10px">
+              <Image
+                src={image}
+                height="300px"
+                width="200px"
+                className="contain"
+              />
+            </Absolute>
+          </Flex>
+        </Relative>
       }
     >
       <Text f="36px" className="hoverable cursive" fontWeight="700">
